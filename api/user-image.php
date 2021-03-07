dbbc51db7fd883d0426b7e06c653fec57e57b728
Yusril A. P <yusril_putra56@yahoo.co.id>
2021-03-07 16:07:06 +0700

    add change image functionality

--- api/user-image.php
+++ api/user-image.php
@@ -4,7 +4,26 @@
   $request = json_decode(file_get_contents('php://input'), true);
   $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-  if ($requestMethod !== "PUT") {
+  if(isset($request['setDefault'])) {
+    $user = new User($_SESSION['username']);
+    $info = $user->getUser();
+
+    $defaultFileName = 'default_user.png';
+
+    if($info['image'] !== $defaultFileName) {
+      unlink("../public/img/users/{$info['image']}");
+    }
+
+    $user->updateImage($defaultFileName);
+
+    $response['status'] = 'success';
+    $response['message'] = 'Image updated.';
+    $response['filename'] = $defaultFileName;
+    echo json_encode($response);
+    exit;
+  }
+
+  if ($requestMethod !== "POST") {
     errorResponse('This request method is not supprted for this endpoint.', 405);
   }
 
@@ -12,12 +31,12 @@
     unauthorizedResponse();
   }
 
-  if(!isset($_FILES['profil_image'])) {
+  if(!isset($_FILES['profile_image'])) {
     errorResponse('Image is empty or not uploaded.', 428);
   }
 
   try {
-    $imgFile = $_FILES['profil_image'];
+    $imgFile = $_FILES['profile_image'];
 
     $allowedImgExtension = ["jpg",  "jpeg", "png"];
     $extension = explode('.', $imgFile['name']);
@@ -29,15 +48,24 @@
     }
 
     $user = new User($_SESSION['username']);
-    $id = $user->getUser()['id'];
+    $info = $user->getUser();
 
-    $newFileName .= "{$id}.{$extension}";
+    $defaultFileName = 'default_user.png';
+
+    if($info['image'] !== $defaultFileName) {
+      unlink("../public/img/users/{$info['image']}");
+    }
+
+    $id = $info['id'];
+    $newFileName = "{$id}.{$extension}";
+    $fileTmp = $imgFile["tmp_name"];
     move_uploaded_file($fileTmp, "../public/img/users/$newFileName");
 
     $user->updateImage($newFileName);
 
     $response['status'] = 'success';
     $response['message'] = 'Image updated.';
+    $response['filename'] = $newFileName;
     echo json_encode($response);
   } catch (Exception $error) {
     errorResponse($error->getMessage());
