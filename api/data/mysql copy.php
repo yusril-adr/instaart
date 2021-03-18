2097224139adba96a5d0d027e7f4e88aeb7f1088
Yusril A. P <yusril_putra56@yahoo.co.id>
2021-03-19 00:20:20 +0700

    repair db connection

--- api/data/mysql copy.php
+++
@@ -1,8 +0,0 @@
-<?php
-  const DB_BASE = 'localhost';
-  const DB_USER = 'root';
-  const DB_PASSWORD = '';
-  const DB_NAME = 'instaart';
-
-  $conn = mysqli_connect(DB_BASE, DB_USER, DB_PASSWORD, DB_NAME);
-?>
