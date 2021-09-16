979e51d79ed871c2f1e7c63014ea9c57c716fd33
Yusril A. P <yusriladr.37@gmail.com>
2021-09-16 21:10:37 +0700

    Add activities API endpoint

---
+++ api/activities.php
@@ -0,0 +1,30 @@
+<?php 
+  require_once './utils/import-helper.php';
+  if (isset($_SERVER['HTTP_X_AUTH_ID'])) {
+    $authId = $_SERVER['HTTP_X_AUTH_ID'];
+  }
+  if (isset($_SERVER['HTTP_X_AUTH_TOKEN'])) {
+    $authToken = $_SERVER['HTTP_X_AUTH_TOKEN'];
+  }
+
+  if($_SERVER["REQUEST_METHOD"] !== 'GET') {
+    errorResponse('This request method is not supprted for this endpoint.', 405);
+  }
+
+  if(!isset($authId) || !checkToken($authToken, $authId)) unauthorizedResponse();
+
+  try {
+    $username = User::getUserFromId($authId)['username'];
+    $user = new User($username);
+    
+    $response = $user->getActivites();
+
+    echo json_encode($response);
+    exit;
+  } catch (Exception $error) {
+    if($error->getCode() !== 0) {
+      errorResponse($error->getMessage(), $error->getCode());
+    }
+    errorResponse($error->getMessage());
+  }
+?>
