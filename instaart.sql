445690098c4c72ae1a47f0401dfef221653b2d07
Yusril A. P <yusriladr.37@gmail.com>
2021-09-18 09:06:41 +0700

    Update sql file

--- instaart.sql
+++ instaart.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2021 at 02:58 PM
--- Server version: 10.4.16-MariaDB
--- PHP Version: 7.4.12
+-- Generation Time: Sep 18, 2021 at 04:04 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,83 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookmark_posts`
+--
+
+CREATE TABLE `bookmark_posts` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bookmark_posts`
+--
+
+INSERT INTO `bookmark_posts` (`id`, `post_id`, `user_id`) VALUES
+(20, 7, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Arsitektur'),
+(2, 'Fashion'),
+(3, 'Desain Grafis'),
+(4, 'Ilustrasi'),
+(5, 'Desain Web');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `colors`
+--
+
+CREATE TABLE `colors` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `colors`
+--
+
+INSERT INTO `colors` (`id`, `name`) VALUES
+(1, 'Merah'),
+(2, 'Jingga'),
+(3, 'Kuning'),
+(4, 'Hitam'),
+(5, 'Biru'),
+(6, 'Putih'),
+(7, 'Hijau');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `comments`
 --
 
@@ -32,8 +109,18 @@ CREATE TABLE `comments` (
   `post_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   `body` text DEFAULT NULL,
-  `date` datetime DEFAULT current_timestamp()
+  `date` datetime DEFAULT current_timestamp(),
+  `type` varchar(10) NOT NULL DEFAULT 'comment'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `post_id`, `user_id`, `body`, `date`, `type`) VALUES
+(2, 7, 5, 'Mantap mas', '2021-09-16 18:35:00', 'comment'),
+(3, 9, 5, 'boleh boleh', '2021-09-16 18:47:06', 'comment'),
+(4, 7, 1, 'hehe', '2021-09-16 18:48:24', 'comment');
 
 -- --------------------------------------------------------
 
@@ -44,8 +131,43 @@ CREATE TABLE `comments` (
 CREATE TABLE `follows` (
   `id` int(11) NOT NULL,
   `following_id` int(11) DEFAULT NULL,
-  `follower_id` int(11) DEFAULT NULL
+  `follower_id` int(11) DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `type` varchar(10) NOT NULL DEFAULT 'follow'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `follows`
+--
+
+INSERT INTO `follows` (`id`, `following_id`, `follower_id`, `date`, `type`) VALUES
+(2, 1, 5, '2021-09-16 18:35:36', 'follow');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `province_id` int(11) NOT NULL,
+  `province_name` varchar(100) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  `city_name` varchar(100) NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL,
+  `work_time` varchar(10) NOT NULL,
+  `form_link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `jobs`
+--
+
+INSERT INTO `jobs` (`id`, `user_id`, `province_id`, `province_name`, `city_id`, `city_name`, `title`, `description`, `work_time`, `form_link`) VALUES
+(2, 1, 35, 'Jawa Timur', 3578, 'Kota Surabaya', 'Front End Web Dev', 'Menguasai :\n- HTML\n- CSS\n- JS', 'Full Time', 'https://google.com');
 
 -- --------------------------------------------------------
 
@@ -59,6 +181,14 @@ CREATE TABLE `likes` (
   `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`id`, `post_id`, `user_id`) VALUES
+(23, 9, 1),
+(24, 7, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -68,10 +198,33 @@ CREATE TABLE `likes` (
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
+  `category_id` int(11) NOT NULL,
+  `color_id` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `caption` text DEFAULT NULL,
   `image` varchar(25) DEFAULT NULL,
-  `date` datetime DEFAULT current_timestamp()
+  `date` datetime DEFAULT current_timestamp(),
+  `insight` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `category_id`, `color_id`, `title`, `caption`, `image`, `date`, `insight`) VALUES
+(7, 1, 4, 4, 'Logo', 'a', '613eac27e030e.png', '2021-09-13 08:40:55', 16),
+(9, 1, 1, 5, 'Desain 2', 'a', '61431638465ae.png', '2021-09-16 17:02:32', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tokens`
+--
+
+CREATE TABLE `tokens` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `token` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -88,12 +241,50 @@ CREATE TABLE `users` (
   `biodata` text NOT NULL,
   `image` varchar(25) NOT NULL DEFAULT 'default_user.png',
   `email` varchar(35) NOT NULL,
-  `phone_number` varchar(20) NOT NULL
+  `phone_number` varchar(20) NOT NULL,
+  `province_id` int(11) NOT NULL,
+  `province_name` varchar(100) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  `city_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `display_name`, `biodata`, `image`, `email`, `phone_number`, `province_id`, `province_name`, `city_id`, `city_name`) VALUES
+(1, 'yusril-adr', '$2y$10$xv7h17anyHctC8H5Uu/wEOPfeP67hspahIPCsPJE3HiNhLHOU18mm', 'Yusril A. P.', 'a', '1.jpg', 'a@a', '087854029394', 35, 'Jawa Timur', 3578, 'Kota Surabaya'),
+(5, 'yusril_adr', '$2y$10$TAZ/gd/4rw/.nYktUIFT5ORyeKLzRqjpYkNMK6Rs5ui9breXxP4hm', 'Yusril', '', 'default_user.png', 'b@b', '087854029394', 19, 'Kepulauan Bangka Belitung', 1905, 'Kabupaten Bangka Selatan');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bookmark_posts`
+--
+ALTER TABLE `bookmark_posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `colors`
+--
+ALTER TABLE `colors`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `comments`
@@ -112,6 +303,13 @@ ALTER TABLE `follows`
   ADD KEY `follower_id` (`follower_id`);
 
 --
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `likes`
 --
 ALTER TABLE `likes`
@@ -123,6 +321,15 @@ ALTER TABLE `likes`
 -- Indexes for table `posts`
 --
 ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `color_id` (`color_id`);
+
+--
+-- Indexes for table `tokens`
+--
+ALTER TABLE `tokens`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
@@ -139,38 +346,81 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `bookmark_posts`
+--
+ALTER TABLE `bookmark_posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `colors`
+--
+ALTER TABLE `colors`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `follows`
 --
 ALTER TABLE `follows`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tokens`
+--
+ALTER TABLE `tokens`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `bookmark_posts`
+--
+ALTER TABLE `bookmark_posts`
+  ADD CONSTRAINT `bookmark_posts_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  ADD CONSTRAINT `bookmark_posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `comments`
@@ -187,6 +437,12 @@ ALTER TABLE `follows`
   ADD CONSTRAINT `follows_ibfk_2` FOREIGN KEY (`follower_id`) REFERENCES `users` (`id`);
 
 --
+-- Constraints for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD CONSTRAINT `jobs_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
 -- Constraints for table `likes`
 --
 ALTER TABLE `likes`
@@ -197,7 +453,15 @@ ALTER TABLE `likes`
 -- Constraints for table `posts`
 --
 ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `posts_ibfk_3` FOREIGN KEY (`color_id`) REFERENCES `colors` (`id`);
+
+--
+-- Constraints for table `tokens`
+--
+ALTER TABLE `tokens`
+  ADD CONSTRAINT `tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
